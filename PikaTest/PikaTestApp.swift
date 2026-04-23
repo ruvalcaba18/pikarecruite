ed5efd924970b93cdb1da345cf30da959bbccbe0
Jael <jael.ruvalcaba@uabc.edu.mx>
2026-04-23 13:55:48 -0700

    Initial Commit

---
+++ PikaTest/PikaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikaTestApp.swift
+//  PikaTest
+//
+//  Created by jael ruvalcaba on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PikaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
